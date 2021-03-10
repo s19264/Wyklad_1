687f25913477991e15076bb37b5730b6f5e6762c
Stanislaw Kubik <s19264@pjwstk.edu.pl>
2021-03-10 21:34:14 +0100

    dodale pierwszy projekt

---
+++ PJATK/Crawler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Crawler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
