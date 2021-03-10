824188afc37b79cebb1e7536a031056786acea5d
Stanislaw Kubik <s19264@pjwstk.edu.pl>
2021-03-10 21:56:57 +0100

    dodalem message 2

--- PJATK/Crawler/Program.cs
+++ PJATK/Crawler/Program.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Crawler
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            HttpClient httpClient = new HttpClient();
+            if (args.Length == 0)
+            {
+                throw new ArgumentException("nie mam argumentu");
+            }
+
+            string url = args[0];
+
+            using (var httpClient = new HttpClient()) { 
+
+            //Crawler
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                //Tutaj będzie potrzebny Regex!!!!!!!!!
+                Console.WriteLine(content);
+            }
+
+
+            
+        }
+
             Console.WriteLine("Hello World!");
         }
     }
